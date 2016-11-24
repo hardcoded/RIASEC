9bb994b9195f2652db446a47d09d757ec12ba4f4
Riviere Tristan <northriver@laposte.net>
2016-11-24 14:43:51 +0100

    Ajout de la structure (avant ca avais raté)
    J'ai mis une structure MCD de base avec une convention d'écriture pour le nom des fichiers c_nom pour controleur. v_nom vue etc

---
+++ html/Fichier Kevin/modules/pagejade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html >
+	<head>
+		<meta "UTF-8">
+		<title>Super page de la mort qui tue</title>
+	</head>
+	<body>
+	<div id = "Titre">
+		Jade's World ! 
+	<div id="menu">
+		<a href="../index.php"> home </a><br>
+		<a href="https://verrue.ooreka.fr/comprendre/verrue-plantaire"> lien tout à fait intéressant</a><br>
+		<a href="http://www.youporn.com"> lien tout à fait intéressant aussi</a><br>
+		<a href="essai2.php"> autre essai avec du css ?</a><br>
+	<div id ="articles">
+		<h1>TITRE ARTICLE</h1>
+		<p>blablablabla ... <strong>bla !</strong></p>
+
+		<p> mais ... <em>blablablabla</em></p>
+
+
+
+</html>
---
+++ html/Fichier Kevin/modules/pagephp.php
@@ -0,0 +1,30 @@
+﻿<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8" />
+		<link rel="stylesheet" type="text/css" href="style.css">
+		<title>SandBox World</title>
+	</head>
+	<body>
+
+<a href="../">Retour</a></br>
+<?php
+echo'Voila du texte affiché en php!</br>';
+$i=0;
+while($i!=10){
+	echo''.$i.'</br>';
+	$i=$i+1;
+}
+
+echo'Voila du html affiché en php!</br>';
+$i=0;
+while($i!=10){
+	echo'<div id="left-bar" style="display:inline-block; color:red;">Je suis le liens '.$i.'</div>';
+	$i=$i+1;
+}
+
+?>
+
+
+	</body>
+</html>
