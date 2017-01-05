4f571607a3304398965602da482d36de617da6a9
Flectra <jhennebert@hotmail.fr>
2017-01-05 16:12:39 +0100

    add hexagon folder

---
+++ hexagon/hexagone.js
@@ -0,0 +1,40 @@
+var ctx = document.getElementById("hexagone");
+
+	
+	var data = {
+	        labels: ["Réaliste", "Investigatif", "Artistique", "Social", "Entrepreneur", "Conventionnel"],
+	        datasets: [{
+	            label: 'Mes Résultats',
+	            backgroundColor: "rgba(179,181,198,0.2)",
+	            borderColor: "rgba(179,181,198,1)",
+	            pointBackgroundColor: "rgba(179,181,198,1)",
+	            pointBorderColor: "#fff",
+	            pointHoverBackgroundColor: "#fff",
+	            pointHoverBorderColor: "rgba(179,181,198,1)",
+	            data: [50, 19, 3, 5, 2, 3],
+	        },
+	        {
+	            label: "Résultats Promo",
+	            backgroundColor: "rgba(255,99,132,0.2)",
+	            borderColor: "rgba(255,99,132,1)",
+	            pointBackgroundColor: "rgba(255,99,132,1)",
+	            pointBorderColor: "#fff",
+	            pointHoverBackgroundColor: "#fff",
+	            pointHoverBorderColor: "rgba(255,99,132,1)",
+	            data: [28, 48, 40, 19, 96, 27]
+	        }]
+	};
+	
+
+	var myRadarChart = new Chart(ctx, {
+	    type: 'radar',
+	    data : data,
+	    options: {
+	        scale: {
+	            reverse: false,
+	            ticks: {
+	            	beginAtZero: true
+	        	}
+	        }
+		}
+	});
