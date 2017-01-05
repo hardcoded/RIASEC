23a7de4084f87f19d8e4dd4c8b68b2e2664d4262
Johan Brunet <brunet.johan.23@gmail.com>
2017-01-05 22:38:52 +0100

    [ADD] Create models

--- html/model/Model.php
+++ html/model/Model.php
@@ -1,7 +1,51 @@
 <?php
 
+require_once "DatabaseConnection.php";
+
 abstract class Model {
 
+    protected $database;
+    protected $pk_key;
+    protected $table;
+
+    protected function query($sql, $params = null) {
+      $database = DatabaseConnection::getInstance();
+      if ($params == null) {
+        $resultat = $this->database->query($sql);    // exécution directe
+      }
+      else {
+        $resultat = $this->database->prepare($sql);  // requête préparée
+        $resultat->execute($params);
+      }
+      return $resultat;
+    }
+
+    public function getAll() {
+      try{
+        $sql = 'SELECT * FROM '.$this->table;
+        $req = $this->query($sql);
+        $res = $req->fetchAll(PDO::FETCH_ASSOC);
+        return $res;
+      }
+      catch (PDOException $e)
+      {
+        echo($e->getMessage());
+        die("<br> Erreur lors de la recherche de tous les objets de la table" . $this->table);
+      }
+    }
+
+    public function getById($id) {
+      try{
+        $sql = 'SELECT * FROM '.$this->table.' WHERE '.$this->pk_key.' = :id';
+        $req = $this->query($sql,array(":id"=>$id));
+        $res = $req->fetch(PDO::FETCH_ASSOC);
+        return $res;
+      }
+      catch(PDOException $e){
+        echo($e->getMessage());
+        die("<br> Erreur lors de la selection de l'objet dans la table " . $this->table);
+      }
+    }
 }
 
 ?>
