63cf616d66b9072962178d5c200ed5f15346597b
Johan Brunet <brunet.johan.23@gmail.com>
2017-01-05 22:39:47 +0100

    Update file

--- html/model/DatabaseConnection.php
+++ html/model/DatabaseConnection.php
@@ -1,4 +1,5 @@
 <?php
+
 // Classe de connexion à une base de données
 // S'inspire du pattern singleton pour n'ouvrir qu'une seule connexion
 // Utilisation :
@@ -7,8 +8,8 @@ class DatabaseConnection {
 
    // Paramètres pour l'accès à la base
    static private $host = "db648137671.db.1and1.com";
-   static private $base = "dbo648137671";
-   static private $user = "db648137671";
+   static private $database   = "db648137671";
+   static private $user = "dbo648137671";
    static private $password = "piscineRiasec";
 
    static private $pdo = null; // Le singleton
@@ -16,7 +17,7 @@ class DatabaseConnection {
    // Obenir le singleton
    static function getInstance() {
       if (self::$pdo == null) {
-         $dsn = sprintf("mysql:host=%s;dbname=%s", self::$host, self::$base);
+         $dsn = sprintf("mysql:host=%s;dbname=%s", self::$host, self::$database);
          try {
             self::$pdo = new PDO($dsn, self::$user, self::$password,
                                  array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
