5689f1fd9613b44fb46592b9300bf38fa3966a68
Johan Brunet <brunet.johan.23@gmail.com>
2017-01-09 17:55:48 +0100

    [UPDATE] Minor updates

--- html/model/PropositionModel.php
+++ html/model/PropositionModel.php
@@ -8,28 +8,30 @@
     protected $pk_key_grp = 'groupe';
     protected $table = 'proposition';
 
-    function editLabel($id, $group, $newLabel) {
+    public function editLabel($id, $group, $newLabel) {
       try {
-        $sql = 'UPDATE '.$this->table.' SET label_proposition = :newLabel WHERE '.$this->pk_key_id.' = :id AND '.$this->pk_key_grp.' = :group;';
+        $sql = "UPDATE ".$this->table." SET label_proposition = :newLabel WHERE ".$this->pk_key_id." = :id AND ".$this->pk_key_grp." = :group;";
         $this->query($sql, array(":newLabel" => $newLabel,
                                  ":id" => $id,
                                  ":group" => $group));
       }
       catch (PDOException $e) {
         echo($e->getMessage());
-        die("<br> Erreur lors de la recherche de tous les objets de la table" . $this->table);
+        die("<br> Erreur lors de la mise à jour de la table " .$this->table);
       }
     }
-  }
 
-  function getByGroup($group) {
+  public function getByGroup($group) {
     try {
-      $sql = 'SELECT * FROM '.$this->table.' WHERE '.$this->pk_key_group.' = :group;';
-      $this->query($sql, array(":group" => $group));
+      $sql = "SELECT * FROM ".$this->table." WHERE ".$this->pk_key_grp." = :group";
+      $req = $this->query($sql,array(":group"=> $group));
+      $res = $req->fetchAll(PDO::FETCH_ASSOC);
+      return $res;
     }
-    catch (PDOException $e) {
-      echo($e->getMessage());
-      die("<br> Erreur lors de la recherche de tous les objets de la table" . $this->table);
+    catch(PDOException $e){
+      exit('<p>Erreur lors de la selection de l\'objet dans la table : '.$this->table
+           .'<br/>'.$e->getMessage().'</p>');
     }
   }
+}
 ?>
