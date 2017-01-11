542531bf56d8c4e64b5aec82cac9e2a95bea22d5
Johan Brunet <brunet.johan.23@gmail.com>
2017-01-11 16:20:29 +0100

    [ADD] Functions in model for "prom" table

--- html/model/PromModel.php
+++ html/model/PromModel.php
@@ -7,5 +7,22 @@
     protected $pk_key = 'ID_prom';
     protected $table = 'prom';
 
+    public function createProm($prom) {
+      try {
+        $sql = "INSERT INTO ".$this->table." (department, year_prom, graduation_year) VALUES (:dep, :year, :grad)";
+        $req = $this->query($sql, array(':dep' => $prom['department'],
+                                        ':year' => $prom['year'],
+                                        ':grad' => $prom['graduation']));
+      }
+      catch(PDOException $e){
+        exit('<p>Erreur lors de l\'insertion des données dans la table : '.$this->table
+             .'<br/>'.$e->getMessage().'</p>');
+      }
+    }
+
+    public function deleteProm($promID) {
+      $sql = "DELETE FROM ".$this->table." WHERE ".$pk_key." = :promID";
+      $req = $this->query($sql, array(':promID' => $promID));
+    }
   }
 ?>
