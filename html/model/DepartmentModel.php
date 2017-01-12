4a46a64bae5232950bf3518d41110c043652590c
Johan Brunet <brunet.johan.23@gmail.com>
2017-01-12 17:01:48 +0100

    [ADD] Functions for models

--- html/model/DepartmentModel.php
+++ html/model/DepartmentModel.php
@@ -7,5 +7,29 @@
     protected $pk_key = 'ID_department';
     protected $table = 'department';
 
+    public function editLabel($id, $newLabel) {
+      try {
+        $sql = 'UPDATE '.$this->table.' SET label_department = :label WHERE '.$this->pk_key.' = :ID_department';
+        $req = $this->query($sql, array(':label' => $newLabel,
+                                        ':ID_department' => $id));
+      }
+      catch (PDOException $e) {
+        exit('<p>Erreur lors de la mise à jour dans la table : '.$this->table
+             .'<br/>'.$e->getMessage().'</p>');
+      }
+    }
+
+    public function getByLabel($label) {
+      try {
+        $sql = "SELECT * FROM ".$this->table." WHERE label_department = :label";
+        $req = $this->query($sql,array(":label"=> $label));
+        $res = $req->fetch(PDO::FETCH_ASSOC);
+        return $res;
+      }
+      catch(PDOException $e){
+        exit('<p>Erreur lors de la selection de l\'objet dans la table : '.$this->table
+             .'<br/>'.$e->getMessage().'</p>');
+      }
+    }
   }
 ?>
--- html/model/ProfileModel.php
+++ html/model/ProfileModel.php
@@ -7,5 +7,29 @@
     protected $pk_key = 'ID_profile';
     protected $table = 'profile';
 
+    public function getByType($type) {
+      try {
+        $sql = "SELECT * FROM ".$this->table." WHERE type = :type";
+        $req = $this->query($sql,array(":type"=> $type));
+        $res = $req->fetch(PDO::FETCH_ASSOC);
+        return $res;
+      }
+      catch(PDOException $e){
+        exit('<p>Erreur lors de la selection de l\'objet dans la table : '.$this->table
+             .'<br/>'.$e->getMessage().'</p>');
+      }
+    }
+
+    public function updateUrl($id, $newUrl) {
+      try {
+        $sql = 'UPDATE '.$this->table.' SET url_description = :url WHERE '.$this->pk_key.' = :id';
+        $req = $this->query($sql, array(':url' => $newUrl,
+                                        ':id' => $id));
+      }
+      catch (PDOException $e) {
+        exit('<p>Erreur lors de la mise à jour dans la table : '.$this->table
+             .'<br/>'.$e->getMessage().'</p>');
+      }
+    }
   }
 ?>
