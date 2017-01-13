009fe734c3b4743e7794e9846d1ee1e863ac7e5b
Kevin giordani <giordani.kevin.02@gmail.com>
2017-01-13 12:06:02 +0100

    Add files via upload

--- html/index.php
+++ html/index.php
@@ -1,12 +1,18 @@
-<?php
-
+<?php 
+	include_once('connexion_sql.php');
+	
 	if(!isset($_GET['section']) || $_GET['section']=='index'){
-		require_once('controleur/c_connection.php');
-    // require_once('vue/v_connection.php');
+		include_once('controleur/c_connection.php');
 	}
 	else if($_GET['section']=='qcm'){
 		include_once('controleur/c_qcm.php');
 	}
 	else if($_GET['section']=='compte'){
 		include_once('controleur/c_compte.php');
+	}
+	else if($_GET['section']=='resultats'){
+		include_once('controleur/c_resultats.php');
+	}
+	else if($_GET['section']=='profils'){
+		include_once('controleur/c_profils.php');
 	}
