2c265dbf91f6d977ac46ebe688342605925bc078
Kevin giordani <giordani.kevin.02@gmail.com>
2017-01-15 15:18:50 +0100

    Add files via upload

--- html/controleur/c_resultats.php
+++ html/controleur/c_resultats.php
@@ -1,6 +1,11 @@
 <?php
 
 	include_once('modele/m_resultats.php');
-
-	$resultat = [6,46,56,1,5,35];
+	$resultat;
+	if(!isset($_GET['r'])){
+		//Dans la bd
+	}
+	else if(isset($_GET['r']) && isset($_GET['i']) && isset($_GET['a']) &&isset($_GET['s']) && isset($_GET['e']) && isset($_GET['c'])){
+		$resultat = [intval($_GET['r']),intval($_GET['i']),intval($_GET['a']),intval($_GET['s']),intval($_GET['e']),intval($_GET['c'])];
+	}
 	include_once('vue/v_resultats.php');
