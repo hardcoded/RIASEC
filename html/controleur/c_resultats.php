8933b254f68cf40d375019406c94f1785433f192
Unknown <jhennebert@hotmail.fr>
2017-01-15 16:13:32 +0100

    stockage et récupération résultat
    j'ai codé le stockage et la récupération de résultats dans la base de donnée pour l'affichage des résultats

--- html/controleur/c_resultats.php
+++ html/controleur/c_resultats.php
@@ -1,11 +1,23 @@
 <?php
-
-	include_once('modele/m_resultats.php');
-	$resultat;
+	include_once('includes/table.js');
+	include_once('modele/ResultModel.php');
+	$resultat; 
+	$IDstudent = $authController-->decodeToken($_COOKIE["token"]["data"]["userID"]);
 	if(!isset($_GET['r'])){
-		//Dans la bd
+		//if (faire un cas où base de donnée vide)		
+		$resultat =getByStudent($IDstudent);}
 	}
 	else if(isset($_GET['r']) && isset($_GET['i']) && isset($_GET['a']) &&isset($_GET['s']) && isset($_GET['e']) && isset($_GET['c'])){
 		$resultat = [intval($_GET['r']),intval($_GET['i']),intval($_GET['a']),intval($_GET['s']),intval($_GET['e']),intval($_GET['c'])];
+		$resultat_stock =[ 
+		array('type' => 'R','percentage' =>$resultat[0]);
+		array('type' => 'I','percentage' => $resultat[1]);
+		array('type' => 'A','percentage' => $resultat[2]);
+		array('type' => 'S','percentage' => $resultat[3]);
+		array('type' => 'E','percentage' => $resultat[4]);
+		array('type' => 'C','percentage' => $resultat[5]);
+		storeResult($IDstudent,$resultat_stock)
+
 	}
+
 	include_once('vue/v_resultats.php');
