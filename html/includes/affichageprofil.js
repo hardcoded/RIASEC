3cce1056790b1a2437e3bbf75af0d6b5734fd3f3
alexaufauvre <alex.aufauvre@etu.umontpellier.fr>
2017-01-16 19:27:25 +0100

    Add files via upload

--- html/includes/affichageprofil.js
+++ html/includes/affichageprofil.js
@@ -1,17 +1,17 @@
 // Affiche un message avec le profil calculé de l'utilisateur.
-var score= Algorithme();
+// var score= Algorithme();
 
 
 function Profil(score){
-	var profils=["réaliste","investigatif","artistique","social","entrepreneur","conventionnel"];
+	var profils=["realiste","investigatif","artistique","social","entrepreneur","conventionnel"];
 	var max = score[0];
-	int j = 0;
+	var j = 0;
 	for (var i=0; i<score.length-1; i++){
-		if score[i+1]>max{
+		if (score[i+1]>max){
 			max = score[i+1];
 			j=i+1;
 		} 
 	}
 	//alert("D'après vos réponses au questionnaire RIASEC, Vous avez un profil de type"+profils[j]);
 	return profils[j];
-	};
+};
