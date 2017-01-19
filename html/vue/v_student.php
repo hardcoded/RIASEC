8e90fd3a610991af8aabf0ea44e5803b650277bf
Riviere Tristan <northriver@laposte.net>
2017-01-19 16:12:06 +0100

    Commentaires

--- html/vue/v_student.php
+++ html/vue/v_student.php
@@ -6,11 +6,8 @@
 		<title>Mon Compte - Riasec</title>
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
 		<script src="includes/jquery-3.1.1.min.js"></script>
-		<!-- Latest compiled and minified CSS -->
 		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
-		<!-- Optional theme -->
 		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
-		<!-- Latest compiled and minified JavaScript -->
 		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
 		<script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.4.0/Chart.bundle.js"></script>
 		<script src="includes/hexagone.js"></script>
@@ -19,6 +16,7 @@
 	</head>
 	<body style="background-color: #34495e;">
 
+	<!-- Bar suppérieur pour la déconnection -->
     <div class="navbar navbar-inverse" style="margin-bottom: 0px" role="navigation">
       <div class="container-fluid">
           <div class="navbar-header">
@@ -42,13 +40,15 @@
 				echo $error;
 			}
 		?>
-
+		<!-- Bar latéral de bouttons -->
 		<div class="buttonHandlerProfil">
 			<?php
 				echo'<span type="button" class="bouttonProfil" data-toggle="modal" data-target="#modalJoin">Rejoindre session</span>';
 			?>
 			<div id='message' style="color: white"></div>
 		</div>
+
+		<!-- Corp de la page -->
 		<div class="histoHandlerProfil" style="margin-top: 20px">
 			<div id='stat' style='max-width:500px;margin:0 auto' class='swipe'>
 			    <div class='swipe-wrap'>
@@ -69,7 +69,7 @@
 			    </div>
 
 			    <script>var ar = <?php echo json_encode($resArray) ?> </script>
-
+			    <!-- Script d'affichage de l'hexagone -->
 				<script src='includes/swipe.js'></script>
 				<script>drawBar(ar); drawHexagone(ar);</script>
 
@@ -79,7 +79,7 @@
 
 		 <button onclick='change()' class="button" style="vertical-align:middle"><span>Change </span></button>
 		</div>
-
+		<!-- Script pour l'affichage du lien vers le profils prépondérent -->
 		<script>
 			var profil = Profil(ar);
 			document.getElementById('message').innerHTML = 'D\'après vos réponses au questionnaire RIASEC, vous avez un profil de type <a href="?section=profils&profil=' + profil + '" style="color:white; font-size:1,2em;">' + profil + '</a>';
